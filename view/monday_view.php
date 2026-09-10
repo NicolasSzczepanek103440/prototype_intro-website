f6568ac46d05e98c2eda56ed8318699e2208efee
phantomghost910 <103609@glr.nl>
2026-09-10 11:12:31 +0200

    commit

---
+++ view/monday_view.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="../css/navbar.css">
+    <link rel="stylesheet" href="../css/animation.css">
+    <link rel="stylesheet" href="../css/days.css">
+    <script src="../js/days.js" defer></script>
+</head>
+<body>
+    
+</body>
+</html>
